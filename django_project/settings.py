b521b0c0dfc0e80a40a0ec0ef3619e3f4a226f22
ndimangwa <ndimangwa@gmail.com>
2025-11-11 11:50:36 +0300

    Added zoomtong.com

--- django_project/settings.py
+++ django_project/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ecommerce-production-753f.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://ecommerce-production-753f.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'zoomtong.com', 'ecommerce-production-753f.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://zoomtong.com', 'https://ecommerce-production-753f.up.railway.app']
 
 # Application definition
 
