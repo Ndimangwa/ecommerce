72cde099836fe6e255e1a3e56d00f07f9aab47a2
ndimangwa <ndimangwa@gmail.com>
2025-11-17 11:47:37 +0300

    valid_ip_received(sender, **kwargs)

--- payment/hooks.py
+++ payment/hooks.py
@@ -4,8 +4,10 @@ from django.dispatch import receiver
 from django.conf import settings 
 
 @receiver(valid_ipn_received)
-def paypal_payment_received(sender, ipn, **kwargs):
+def paypal_payment_received(sender, **kwargs):
     #grab the paypal object
-    paypal_obj = ipn
+    paypal_obj = sender
     #printing
     print(paypal_obj)
+    print(paypal_obj.mc_gross)
+    print(f'Amount paid is : {paypal_obj.mc_gross}')
